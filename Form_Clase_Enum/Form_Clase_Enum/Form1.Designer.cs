930157af0bc028b10829263bf66eb7787447ae9c
Alejandro Bagnat (CM) <alejandro@bsg.global>
2018-10-10 13:08:15 -0300

    Second commit

---
+++ Form_Clase_Enum/Form_Clase_Enum/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Aceptar = new System.Windows.Forms.Button();
+            this.Btn_Salir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtNumDoc = new System.Windows.Forms.TextBox();
+            this.TxtNota = new System.Windows.Forms.TextBox();
+            this.CmdTipo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Btn_Aceptar
+            // 
+            this.Btn_Aceptar.Location = new System.Drawing.Point(55, 156);
+            this.Btn_Aceptar.Name = "Btn_Aceptar";
+            this.Btn_Aceptar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Aceptar.TabIndex = 0;
+            this.Btn_Aceptar.Text = "Aceptar";
+            this.Btn_Aceptar.UseVisualStyleBackColor = true;
+            this.Btn_Aceptar.Click += new System.EventHandler(this.Btn_Aceptar_Click);
+            // 
+            // Btn_Salir
+            // 
+            this.Btn_Salir.Location = new System.Drawing.Point(143, 156);
+            this.Btn_Salir.Name = "Btn_Salir";
+            this.Btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Salir.TabIndex = 1;
+            this.Btn_Salir.Text = "Salir";
+            this.Btn_Salir.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tipo Documento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nº Documento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nota";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(118, 24);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombre.TabIndex = 6;
+            // 
+            // TxtNumDoc
+            // 
+            this.TxtNumDoc.Location = new System.Drawing.Point(118, 88);
+            this.TxtNumDoc.Name = "TxtNumDoc";
+            this.TxtNumDoc.Size = new System.Drawing.Size(100, 20);
+            this.TxtNumDoc.TabIndex = 8;
+            // 
+            // TxtNota
+            // 
+            this.TxtNota.Location = new System.Drawing.Point(118, 120);
+            this.TxtNota.Name = "TxtNota";
+            this.TxtNota.Size = new System.Drawing.Size(100, 20);
+            this.TxtNota.TabIndex = 9;
+            // 
+            // CmdTipo
+            // 
+            this.CmdTipo.FormattingEnabled = true;
+            this.CmdTipo.Location = new System.Drawing.Point(118, 56);
+            this.CmdTipo.Name = "CmdTipo";
+            this.CmdTipo.Size = new System.Drawing.Size(100, 21);
+            this.CmdTipo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 192);
+            this.Controls.Add(this.CmdTipo);
+            this.Controls.Add(this.TxtNota);
+            this.Controls.Add(this.TxtNumDoc);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_Salir);
+            this.Controls.Add(this.Btn_Aceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Aceptar;
+        private System.Windows.Forms.Button Btn_Salir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtNumDoc;
+        private System.Windows.Forms.TextBox TxtNota;
+        private System.Windows.Forms.ComboBox CmdTipo;
+    }
+}
+
---
+++ Form_Clase_Enum/Form_Clase_Enum/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public class Alumno
+        {
+            public tipodocumento tipodocumento { get; set; }
+           
+
+            public string Nombre { get; set; }
+            public int NumDocumento { get; set; }
+            
+
+            enum tipodocumento  {DNI,cedula,DU };
+            enum notadescripcion {aprobado, desaprobado };
+
+            
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Aceptar_Click(object sender, EventArgs e)
+        {
+            StringBuilder mensaje = new StringBuilder();
+            Alumno Alu = new Alumno();
+          
+
+            Alu.Nombre = TxtNombre.Text;
+            CmdTipo.Items.Add("DNI");
+            CmdTipo.Items.Add("Cedula");
+            CmdTipo.Items.Add("DU");
+            Alu.tipodocumento = CmdTipo.SelectedIndex ;
+            
+            
+         
+           
+            Alu.NumDocumento =int.Parse(TxtNumDoc.Text);
+
+
+
+
+        }
+
+    
+
+     
+    }
+}
